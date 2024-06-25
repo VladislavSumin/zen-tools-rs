ac4ef1dff8fde5e6ee05a42c58781c8e91348280
Владислав <vladislavsumin@mail.ru>
2024-06-25 11:25:31 +0300

    add real requests

--- scripts/allure_test_report_upload_to_influxdb/src/main.rs
+++ scripts/allure_test_report_upload_to_influxdb/src/main.rs
@@ -7,15 +7,29 @@ use core_allure::{AllureDataProvider, parse_allure_report};
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt()
-        .with_max_level(Level::TRACE)
+        .with_max_level(Level::INFO)
         .init();
 
-    let allure_source = AllureFileSource {
-        root_path: PathBuf::from("./allure-reports")
+    let allure_source = AllureNetworkSource {
+        base_url: "https://s3.dzeninfra.ru/zen-mobile-allure/master/13614136/zen/ZenApp/build/artifacts/allure_report/allure-reports".to_owned()
     };
     let tests_info = parse_allure_report(&allure_source).await;
 
     info!("Tests:{:#?}", tests_info);
+}
+
+#[derive(Clone)]
+struct AllureNetworkSource {
+    base_url: String,
+}
+
+impl AllureDataProvider for AllureNetworkSource {
+    fn get_file_string<P: AsRef<Path> + Send>(&self, path: P) -> impl Future<Output=String> + Send {
+        let url = format!("{}/{}", &self.base_url, path.as_ref().to_str().unwrap());
+        async {
+            reqwest::get(url).await.unwrap().text().await.unwrap()
+        }
+    }
 }
 
 #[derive(Clone)]
