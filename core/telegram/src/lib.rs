cab1938e593801ddafb8dd93b46f37f309446094
Владислав <vladislavsumin@mail.ru>
2024-06-27 20:12:35 +0300

    add telegram crate

---
+++ core/telegram/src/lib.rs
@@ -0,0 +1,24 @@
+//! # core_telegram
+//! Набор утилит для работы с api telegram
+use teloxide::prelude::*;
+use teloxide::payloads::GetUpdates;
+use teloxide::requests::JsonRequest;
+
+/// Набор расширений для [Bot].
+pub trait TgBotCommands {
+    /// Получает список обновлений начиная с [event_id].
+    /// Обратите внимание, если ранее бот передавал больший [event_id] то телега это запомнит
+    /// и вернет только более новые события.
+    /// Можно передать 0 в качестве [event_id] что бы получить максимум возможных событий.
+    async fn get_updates_start_from(self, event_id: i32) -> Vec<Update>;
+}
+
+impl TgBotCommands for Bot {
+    async fn get_updates_start_from(self, event_id: i32) -> Vec<Update> {
+        let get_updates = GetUpdates {
+            offset: Some(event_id),
+            ..Default::default()
+        };
+        JsonRequest::new(self, get_updates).await.unwrap()
+    }
+}
