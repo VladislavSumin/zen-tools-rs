e752048fc0d53d6b509a835a5cb3be93b7a19554
Владислав <vladislavsumin@mail.ru>
2024-06-29 20:23:51 +0300

    fix cargo warns

--- core/telegram/src/lib.rs
+++ core/telegram/src/lib.rs
@@ -1,5 +1,6 @@
 //! # core_telegram
 //! Набор утилит для работы с api telegram
+
 use teloxide::prelude::*;
 use teloxide::payloads::GetUpdates;
 use teloxide::requests::JsonRequest;
@@ -10,6 +11,7 @@ pub trait TgBotCommands {
     /// Обратите внимание, если ранее бот передавал больший [event_id] то телега это запомнит
     /// и вернет только более новые события.
     /// Можно передать 0 в качестве [event_id] что бы получить максимум возможных событий.
+    #[allow(async_fn_in_trait)]
     async fn get_updates_start_from(self, event_id: i32) -> Vec<Update>;
 }
 
